853635c44c856b6bb348c6bfa313c8ce0f1004c3
Atousa Duprat <atousa.p@gmail.com>
2020-09-02 15:10:19 -0700

    Add files via upload

---
+++ Height.java
@@ -0,0 +1,7 @@
+public class Height {
+    public static int height(TreeNode tree) {
+        if(tree == null) return 0;
+        return Math.max(height(tree.getLeftNode()),height(tree.getRightNode())+1);
+    }
+
+}
