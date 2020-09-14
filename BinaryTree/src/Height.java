3a5550a2e0bbae2180c3d2e6d13bc11ea2ae4cd5
Atousa Duprat <atousa.p@gmail.com>
2020-09-14 12:05:50 -0700

    removed a duplicated line

--- BinaryTree/src/Height.java
+++ BinaryTree/src/Height.java
@@ -1,7 +1,6 @@
 public class Height {
     public static int height(TreeNode tree) {
         if(tree == null) return 0;
-        if(tree == null) return 0;
         return Math.max(height(tree.getLeftNode()),height(tree.getRightNode())+1);
     }
 
