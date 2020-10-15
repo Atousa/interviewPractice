4d39345bbe3f6dd511d2a97ffe2632d734dc423c
Atousa Duprat <atousa.p@gmail.com>
2020-10-15 09:53:18 -0700

    Diameter of a tree

--- BinaryTree/src/Diameter.java
+++ BinaryTree/src/Diameter.java
@@ -1,40 +1,69 @@
 public class Diameter {
+        //Write a function that returns the length of the longest leaf-to-leaf path in a binary tree
+        static class Diam{
+            private int diameter;
+            private int height;
 
-    //Write a function that returns the length of the longest leaf-to-leaf path in a binary tree
-    static class D{
-        int diameter = Integer.MIN_VALUE;
-    }
-    public static int diameter(TreeNode root) {
-        if(root == null) return 0;
-        D diam = new D();
-        height(root, diam);
-        return diam.diameter;
-    }
-    public static int height(TreeNode root,D d) {
-        if(root==null) {
-            d.diameter=0;
-            return 0;
+            public Diam(int d, int h) {
+                this.diameter = d;
+                this.height = h;
+            }
+
+            public int getDiameter() {
+                return diameter;
+            }
+
+            public void setDiameter(int diameter) {
+                this.diameter = diameter;
+            }
+
+            public int getHeight() {
+                return height;
+            }
+
+            public void setHeight(int height) {
+                this.height = height;
+            }
+
         }
-        int leftHeight = height(root.getLeftNode(),d);
-        int rightHeight = height(root.getRightNode(),d);
 
-        d.diameter = Math.max(d.diameter,(leftHeight+rightHeight+1));
+    public static Diam diameter(TreeNode root) {
+        if(root == null) return new Diam(0,0);
 
-        return Math.max(rightHeight,leftHeight)+1;
+        Diam leftDiam = diameter(root.getLeftNode());
+        Diam rightDiam = diameter(root.getRightNode());
+        int height = Integer.max(leftDiam.height,rightDiam.height)+1;
+        int diameter = Integer.max(Integer.max(leftDiam.diameter, rightDiam.diameter), leftDiam.height+rightDiam.height+1);
+        Diam diam = new Diam(diameter , height );
+        return diam;
     }
+
     public static void main(String[] args) {
-        TreeNode t = new TreeNode(1);
-        TreeNode left = new TreeNode(2);
-        t.setLeftNode(left);
-        TreeNode right = new TreeNode(3);
-        t.setRightNode(right);
-        TreeNode left2 = new TreeNode(4);
-        TreeNode right2 = new TreeNode(5);
-        left.setRightNode(right2);
-        left.setLeftNode(left2);
-        TreeNode left3 = new TreeNode(7);
-        left2.setLeftNode(left3);
-        right.setLeftNode(new TreeNode((9)));
-        System.out.println(diameter(t));
+
+
+        TreeNode t0 = new TreeNode(0);
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
+        TreeNode t8 = new TreeNode(8);
+        TreeNode t9 = new TreeNode(9);
+        TreeNode t10 = new TreeNode(10);
+        TreeNode t11 = new TreeNode(11);
+        t0.setLeftNode(t1);
+        t0.setRightNode(t2);
+        t1.setLeftNode(t3);
+        t1.setRightNode(t4);
+        t2.setRightNode(t5);
+        t4.setLeftNode(t6);
+        t4.setRightNode(t7);
+        t5.setRightNode(t8);
+        t8.setLeftNode(t9);
+        t8.setRightNode(t10);
+        t9.setRightNode(t11);
+        System.out.println(diameter_JLD(t0).diameter);
     }
 }
