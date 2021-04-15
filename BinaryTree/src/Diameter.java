4ff9b653dc4a8f0192a5f4baa607deffde304e48
Atousa Duprat <atousa.p@gmail.com>
2021-04-14 17:52:56 -0700

    Diameter of a tree

--- BinaryTree/src/Diameter.java
+++ BinaryTree/src/Diameter.java
@@ -1,69 +1,44 @@
 public class Diameter {
-        //Write a function that returns the length of the longest leaf-to-leaf path in a binary tree
-        static class Diam{
-            private int diameter;
-            private int height;
+    public static int maxDiam = Integer.MIN_VALUE;
 
-            public Diam(int d, int h) {
-                this.diameter = d;
-                this.height = h;
-            }
+    public static int diameter(TreeNode root) {
+        if(root == null)
+            return 0;
 
-            public int getDiameter() {
-                return diameter;
-            }
+        int leftDiam = diameter(root.getLeftNode());
+        int rightDiam = diameter(root.getRightNode());
 
-            public void setDiameter(int diameter) {
-                this.diameter = diameter;
-            }
-
-            public int getHeight() {
-                return height;
-            }
-
-            public void setHeight(int height) {
-                this.height = height;
-            }
-
-        }
-
-    public static Diam diameter(TreeNode root) {
-        if(root == null) return new Diam(0,0);
-
-        Diam leftDiam = diameter(root.getLeftNode());
-        Diam rightDiam = diameter(root.getRightNode());
-        int height = Integer.max(leftDiam.height,rightDiam.height)+1;
-        int diameter = Integer.max(Integer.max(leftDiam.diameter, rightDiam.diameter), leftDiam.height+rightDiam.height+1);
-        Diam diam = new Diam(diameter , height );
-        return diam;
+        int m =  leftDiam + rightDiam;
+        if(m > maxDiam) maxDiam = m;
+        return (Math.max(leftDiam,rightDiam) + 1);
     }
 
     public static void main(String[] args) {
-
-
         TreeNode t0 = new TreeNode(0);
         TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t4 = new TreeNode(4);
-        TreeNode t5 = new TreeNode(5);
-        TreeNode t6 = new TreeNode(6);
-        TreeNode t7 = new TreeNode(7);
-        TreeNode t8 = new TreeNode(8);
-        TreeNode t9 = new TreeNode(9);
-        TreeNode t10 = new TreeNode(10);
-        TreeNode t11 = new TreeNode(11);
+//        TreeNode t2 = new TreeNode(2);
+//        TreeNode t3 = new TreeNode(3);
+//        TreeNode t4 = new TreeNode(4);
+//        TreeNode t5 = new TreeNode(5);
+//        TreeNode t6 = new TreeNode(6);
+//        TreeNode t7 = new TreeNode(7);
+//        TreeNode t8 = new TreeNode(8);
+//        TreeNode t9 = new TreeNode(9);
+//        TreeNode t10 = new TreeNode(10);
+//        TreeNode t11 = new TreeNode(11);
         t0.setLeftNode(t1);
-        t0.setRightNode(t2);
-        t1.setLeftNode(t3);
-        t1.setRightNode(t4);
-        t2.setRightNode(t5);
-        t4.setLeftNode(t6);
-        t4.setRightNode(t7);
-        t5.setRightNode(t8);
-        t8.setLeftNode(t9);
-        t8.setRightNode(t10);
-        t9.setRightNode(t11);
-        System.out.println(diameter_JLD(t0).diameter);
+//        t0.setRightNode(t2);
+//        t1.setLeftNode(t3);
+//        t1.setRightNode(t4);
+//        t2.setRightNode(t5);
+//        t4.setLeftNode(t6);
+//        t4.setRightNode(t7);
+//        t5.setRightNode(t8);
+//        t8.setLeftNode(t9);
+//        t8.setRightNode(t10);
+//        t9.setRightNode(t11);
+        diameter(t0);
+        System.out.println(maxDiam);
     }
+
 }
