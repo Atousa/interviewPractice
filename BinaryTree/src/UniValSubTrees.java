b5df0b0acd7eb8e4e0b93703c9c3cc4f1665ec90
Atousa Duprat <atousa.p@gmail.com>
2021-04-19 13:29:06 -0700

    return the number of uni-value subtrees

---
+++ BinaryTree/src/UniValSubTrees.java
@@ -0,0 +1,40 @@
+public class UniValSubTrees {
+
+    public static int num = 0;
+
+    public static int uniValSubTrees(TreeNode tree) {
+
+        ifUniValSubTreesHelper(tree);
+        return num;
+    }
+
+
+    private static boolean ifUniValSubTreesHelper(TreeNode tree) {
+
+        if (tree.getLeftNode() == null && tree.getRightNode() == null) {
+            num += 1;
+            return true;
+        }
+
+        boolean leftVal = ifUniValSubTreesHelper(tree.getLeftNode());
+        boolean rightVal = ifUniValSubTreesHelper(tree.getRightNode());
+
+        if (leftVal && rightVal) {
+
+            if (tree.getRightNode() == null && tree.getData() == tree.getLeftNode().getData()) {
+                num += 1;
+                return true;
+            } else if (tree.getLeftNode() == null && tree.getData() == tree.getRightNode().getData()) {
+                num += 1;
+                return true;
+            } else if (tree.getData() == tree.getLeftNode().getData() && tree.getData() == tree.getRightNode().getData()) {
+                num += 1;
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
