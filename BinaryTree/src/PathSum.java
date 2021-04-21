68d8e0840b78829d75e5204c7cc84488e6be81bf
Atousa Duprat <atousa.p@gmail.com>
2021-04-20 17:05:30 -0700

    return all root-to-leaf paths where each path's sum equals targetSum

--- BinaryTree/src/PathSum.java
+++ BinaryTree/src/PathSum.java
@@ -16,10 +16,47 @@ public class PathSum {
 
     }
 
+    public static List<List<Integer>> result = new ArrayList<>();
+
+    public static List<List<Integer>> pathSum2(TreeNode root, int targetSum) {
+        List<Integer> slate = new ArrayList<>();
+        pathSumHelper(root,targetSum, slate);
+        return result;
+    }
+    private static void pathSumHelper(TreeNode root, int targetSum, List<Integer> slate) {
+
+        if(root.getLeftNode() == null && root.getRightNode() == null && root.getData() == targetSum) {
+            slate.add(root.getData());
+            List<Integer> l = new ArrayList<>(slate);
+            result.add(l);
+            slate.remove(slate.size()-1);
+            return;
+        }
+
+        slate.add(root.getData());
+        if(root.getLeftNode()!= null) {
+            pathSumHelper(root.getLeftNode(), targetSum - root.getData(), slate);
+        }
+        if(root.getRightNode()!= null) {
+            pathSumHelper(root.getRightNode(), targetSum - root.getData(), slate);
+        }
+        slate.remove(slate.size()-1);
+    }
+
     public static void main(String[] args) {
-        TreeNode tree = new TreeNode(1);
-        tree.setLeftNode(new TreeNode(2));
-        System.out.println(pathSum(tree, 1));
+        TreeNode tree = new TreeNode(5);
+        tree.setLeftNode(new TreeNode(4));
+        tree.setRightNode(new TreeNode(8));
+        tree.getLeftNode().setLeftNode(new TreeNode(11));
+        tree.getLeftNode().getLeftNode().setLeftNode(new TreeNode(7));
+        tree.getLeftNode().getLeftNode().setRightNode(new TreeNode(2));
+        tree.getRightNode().setLeftNode(new TreeNode(13));
+        tree.getRightNode().setRightNode(new TreeNode(4));
+        tree.getRightNode().getRightNode().setLeftNode(new TreeNode(5));
+        tree.getRightNode().getRightNode().setRightNode(new TreeNode(1));
+        //System.out.println(pathSum(tree, 1));
+        pathSum2(tree,22);
+        System.out.println(result);
     }
 
 }
