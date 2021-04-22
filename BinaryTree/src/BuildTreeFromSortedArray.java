72b0c3485801d1d1953a3fd655e7d3f2be77f26a
Atousa Duprat <atousa.p@gmail.com>
2021-04-22 13:37:45 -0700

    build tree from sorted array

---
+++ BinaryTree/src/BuildTreeFromSortedArray.java
@@ -0,0 +1,22 @@
+public class BuildTreeFromSortedArray {
+
+    public static TreeNode buildTreeFromSortedArray(int[] arr) {
+        return helper(arr, 0, arr.length-1);
+    }
+
+    private static TreeNode helper(int[] arr, int start, int end) {
+        if(start > end) return null;
+
+        if(start == end) {
+            TreeNode newNode = new TreeNode(arr[start]);
+            return newNode;
+        }
+
+        int mid = start + (end-start)/2;
+        TreeNode n = new TreeNode(arr[mid]);
+        n.setLeftNode(helper(arr,start, mid-1));
+        n.setRightNode(helper(arr,mid+1,end));
+        return n;
+    }
+
+}
