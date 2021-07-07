7dd55fac6a1a7e8b5d749309331c320a6afa2fec
Atousa Duprat <atousa.p@gmail.com>
2021-07-07 11:32:19 -0700

    find max and min deapth of a tree

---
+++ BinaryTree/src/MinMaxDeapth.java
@@ -0,0 +1,25 @@
+public class MinMaxDeapth {
+
+    public int minDepth(TreeNode root) {
+
+        if(root.left == null && root.right == null) return 1;
+
+        if(root.left == null)
+            return minDepth(root.right) +1;
+        else if(root.right == null)
+            return minDepth(root.left) +1;
+        else
+            return Math.min(minDepth(root.left), minDepth(root.right)) +1;
+
+    }
+
+    public int MaxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+
+    }
+
+}
+
+
+
