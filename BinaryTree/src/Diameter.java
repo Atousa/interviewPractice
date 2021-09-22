bbaa8fb7d33b00b9e1efb7896f87ce2f6754306c
Atousa Duprat <atousa.p@gmail.com>
2021-09-22 15:39:06 -0700

    Compute Diameter of a tree

--- BinaryTree/src/Diameter.java
+++ BinaryTree/src/Diameter.java
@@ -1,49 +1,46 @@
-public class Diameter {
-    public static int maxDiam = Integer.MIN_VALUE;
 
-    public static int height(TreeNode root) {
-        if(root == null)
-            return 0;
+    /*
+        private static class TreeNode{
+            public int label;
+            public TreeNode left_ptr;
+            public TreeNode right_ptr;
+        }
 
-        int leftHeight = height(root.getLeftNode());
-        int rightHeight = height(root.getRightNode());
+        Complete the function below.
+    */
+    static class Diameter{
+        int height;
+        int diam;
+        public Diameter(int h, int d) {
+            this.height = h;
+            this.diam = d;
+        }
+    };
 
-        int maxPathForRoute =  leftHeight + rightHeight;
-        if(maxPathForRoute > maxDiam) maxDiam = maxPathForRoute;
-        return (Math.max(leftHeight,rightHeight) + 1);
+
+    static Integer binary_tree_diameter(TreeNode root) {
+        
+        Diameter dim = binary_tree_diameter_helper(root);
+        return dim.diam;
+
     }
-
-    public static int diameter(TreeNode root) {
-        height(root);
-        return maxDiam;
+    
+    static Diameter binary_tree_diameter_helper(TreeNode root) {
+        if(root == null) {
+            return (new Diameter(0,0));
+        }
+        
+        Diameter left = binary_tree_diameter_helper(root.left_ptr);
+        Diameter right = binary_tree_diameter_helper(root.right_ptr);
+        
+        int max = (left.height + right.height);
+        if(max < left.diam){
+            max = left.diam;
+        }
+        if(max < right.diam){
+            max = right.diam;
+        }
+        return (new Diameter(Integer.max(left.height,right.height)+1, max));
     }
-
-    public static void main(String[] args) {
-        TreeNode t0 = new TreeNode(1);
-        TreeNode t1 = new TreeNode(2);
-//        TreeNode t2 = new TreeNode(2);
-//        TreeNode t3 = new TreeNode(3);
-//        TreeNode t4 = new TreeNode(4);
-//        TreeNode t5 = new TreeNode(5);
-//        TreeNode t6 = new TreeNode(6);
-//        TreeNode t7 = new TreeNode(7);
-//        TreeNode t8 = new TreeNode(8);
-//        TreeNode t9 = new TreeNode(9);
-//        TreeNode t10 = new TreeNode(10);
-//        TreeNode t11 = new TreeNode(11);
-        t0.setLeftNode(t1);
-//        t0.setRightNode(t2);
-//        t1.setLeftNode(t3);
-//        t1.setRightNode(t4);
-//        t2.setRightNode(t5);
-//        t4.setLeftNode(t6);
-//        t4.setRightNode(t7);
-//        t5.setRightNode(t8);
-//        t8.setLeftNode(t9);
-//        t8.setRightNode(t10);
-//        t9.setRightNode(t11);
-        diameter(t0);
-        System.out.println(maxDiam);
-    }
-}
-
+        
+     
